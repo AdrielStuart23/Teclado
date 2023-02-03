81600213352f4d50983bf58aa4bb2b23f5aec0a1
AdrielStuart23 <adrielstuart9@gmail.com>
2023-02-03 17:10:12 -0400

    Efectos de Sonido JS

---
+++ js/control-audio.js
@@ -0,0 +1,25 @@
+  
+
+// sonidos de las teclas menos la de espacio y la de borrar 
+  function PlayControl() {
+    let audio = document.getElementById("audio")
+    audio.play()
+    // sonido de espacio y borrar
+  }
+  function PlayControl2() {
+    let audio = document.getElementById("audio2")
+    audio.play()
+  }
+//   function comenzar() {
+//     let contenido = document.querySelector("main")
+//     contenido.innerHTML = ""
+//   }
+//   comenzar()
+//   function MostrarTeclado() {
+//     let contenido = document.querySelector("main").value()
+//     contenido.innerHTML=(contenido)
+//   }
+//   sonido enviar
+//pendiente 
+//ocultar teclado y presentarlo 
+//escribir
